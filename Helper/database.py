f043e070f4147e340d9aeda3508308865c0316fc
Nick Nilson <nilson.nicholas@gmail.com>
2022-08-09 15:52:06 -0400

    able to connect to database, will further research doing it from project folder with environment variables in the near future

--- Helper/database.py
+++ Helper/database.py
@@ -1,8 +1,8 @@
 # functions to get necessary lists from database.
 # will make a db Class with functions for each of the word criteria.
 # these will return lists
-
-from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy import create_engine, Table
 
 hostname='ZipCoders-MacBook-Pro.local'
 dbname='project_of_passion'
@@ -10,4 +10,12 @@ uname='nick'
 pwd='nick123'
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=hostname, db=dbname, user=uname,pw=pwd))
+metadata = sqlalchemy.MetaData(bind=engine)
+conn = engine.connect()
+
+query = 'SELECT * FROM words'
+words = engine.execute(query)
+for row in words:
+    print(row)
+
 
