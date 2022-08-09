3e0183c0d3e8ced38d133bf998e8872472a10ec0
Nick Nilson <nilson.nicholas@gmail.com>
2022-08-09 10:32:55 -0400

    db file dumped to project folder, setting up access via SQLAlchemy

---
+++ Helper/database.py
@@ -0,0 +1,9 @@
+# functions to get necessary lists from database.
+# will make a db Class with functions for each of the word criteria.
+# these will return lists
+#
+import jinja2
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+
+class data():
