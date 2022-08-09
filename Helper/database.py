0cd4caf50310c626def0ab611724d0e22432db20
Nick Nilson <nilson.nicholas@gmail.com>
2022-08-09 17:17:24 -0400

    able to search database by syllable count

--- Helper/database.py
+++ Helper/database.py
@@ -13,9 +13,38 @@ engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=hos
 metadata = sqlalchemy.MetaData(bind=engine)
 conn = engine.connect()
 
-query = 'SELECT * FROM words'
-words = engine.execute(query)
-for row in words:
-    print(row)
+# query = 'SELECT * FROM words'
+# words = engine.execute(query)
+# for row in words:
+#     print(row)
 
 
+def get_word_details(word):
+    word = word.upper()
+    result = engine.execute(f"SELECT * FROM words WHERE WORD = '{word}'")
+    syllables = 0
+    pronunciation = ''
+    scansion = ''
+    # for item in result:
+    #     print(item)
+    #     print(type(item))
+    result = list(result)[0]
+    return result
+
+
+# def syllable_matches(word):
+#     details = get_word_details(word)
+#     print(len(details))
+#     syllables = details[3]
+#     results = engine.execute(f"SELECT * FROM words WHERE SYLLABLES = {syllables}")
+#     return [get_word_details(result) for result in results]
+
+def syllable_matches(word):
+    details = get_word_details(word)
+    syllables = details[3]
+    results = engine.execute(f"SELECT * FROM words WHERE SYLLABLES = {syllables}")
+    for result in results:
+        print(result)
+
+print(get_word_details('empty'))
+print(syllable_matches('empty'))
