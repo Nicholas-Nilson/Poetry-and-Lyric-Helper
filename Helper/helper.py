3da47b956c3668ad4e96eb1816710d57b0a1b45d
Nick Nilson <nilson.nicholas@gmail.com>
2022-08-11 20:11:04 -0400

    code partially refactored

--- Helper/helper.py
+++ Helper/helper.py
@@ -1,6 +1,13 @@
 import database
+import cryptography
 
 # Functions to sort through lists returned by database.
+
+
+def get_syllables_match_list(word) -> list:
+    db = database
+    word_details = db.get_word_details(word)
+    return db.syllable_matches(word_details)
 
 
 def details_list_to_word_list(list):
@@ -9,10 +16,10 @@ def details_list_to_word_list(list):
 
     #check if try/except is needed for words not in db
     #be sure a dict can be passed to HTML.
-def get_exact_matches(word: str):
+def get_exact_matches(word_details: tuple, syllable_count_matches: list) -> list:
     db = database
-    word_details = db.get_word_details(word)
-    syllable_count_matches = db.syllable_matches(word_details)
+    # word_details = db.get_word_details(word)
+    # syllable_count_matches = db.syllable_matches(word_details)
     rhyme_matches = db.get_rhyme_dict(word_details)
     scansion_matches = db.get_scansion_matches(word_details)
     exact_matches = {}
@@ -31,15 +38,16 @@ def get_exact_matches(word: str):
 #     # print(rhyme_matches[2])
 #     close_matches_rhyme = {}
 
-def get_close_matches_rhyme(word: str) -> dict:
+def get_close_matches_rhyme(word_details: tuple, syllable_count_matches: list) -> dict:
     db = database
-    word_details = db.get_word_details(word)
-    syllable_count_matches = details_list_to_word_list(db.syllable_matches(word_details))
+    # word_details = db.get_word_details(word)
+    # syllable_count_matches = details_list_to_word_list(db.syllable_matches(word_details))
     rhyme_matches = db.get_rhyme_dict(word_details)
     close_matches_rhymes = {}
     for num in range(word_details[3]): # number of syllables
+        syllable_match_list = details_list_to_word_list(syllable_count_matches)
         rhyme_list = details_list_to_word_list(rhyme_matches[num + 1])
-        close_matches_rhymes[num+1] = [word for word in syllable_count_matches if word in rhyme_list]
+        close_matches_rhymes[num+1] = [word for word in syllable_match_list if word in rhyme_list]
         # for word in rhyme_matches[2]:
     #     print(word)
     # for syl in range(word_details[3]):
@@ -53,10 +61,11 @@ def get_close_matches_rhyme(word: str) -> dict:
     return close_matches_rhymes
 
 
-def get_close_matches_scansion(word: str) -> dict:
+def get_close_matches_scansion(word_details: tuple, syllable_count_matches) -> dict:
+    syllable_count_matches = details_list_to_word_list(syllable_count_matches)
     db = database
-    word_details = db.get_word_details(word)
-    syllable_count_matches = details_list_to_word_list(db.syllable_matches(word_details))
+    # word_details = db.get_word_details(word)
+    # syllable_count_matches = details_list_to_word_list(db.syllable_matches(word_details))
     scansion_matches = db.get_scansion_matches(word_details)
     keys = list(scansion_matches.keys())
     # print(keys)
@@ -68,13 +77,20 @@ def get_close_matches_scansion(word: str) -> dict:
     return close_matches_scansion
 
 
+
+word_details_1 = (71786, 'LOVE', 'L AH1 V', 1, "'")
 # rhyme_dict = get_close_matches_rhyme('agitated')
 # scansion = get_close_matches_scansion('agitated')
-print(database.get_word_details('amputated'))
+# print(database.get_word_details('amputated'))
+test = get_syllables_match_list('automatic')
+# print(test)
+rhymes = get_close_matches_rhyme(database.get_word_details('automatic'), test)
+print(rhymes)
+# print(get_close_matches_scansion((71786, 'LOVE', 'L AH1 V', 1, "'"), get_syllables_match_list("love")))
 # scansion = get_close_matches_scansion('verify')
 # print(scansion)
-print(rhyme_dict[3])
-print(scansion)
+# print(rhyme_dict[3])
+# print(scansion)
 # print(scansion[2])
 # print(rhyme_dict)
 # print(details_list_to_word_list(database.syllable_matches(database.get_word_details('customary'))))
